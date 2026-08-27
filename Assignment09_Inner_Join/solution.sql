c76e0309672f1d83303f6991e9b630bb8fc26ad5
1U25CA063-NIRANJANI <niruma3108@gmail.com>
2026-08-27 11:20:11 +0530

    Update solution.sql

--- Assignment09_Inner_Join/solution.sql
+++ Assignment09_Inner_Join/solution.sql
@@ -1,12 +1,21 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-
--- Create Department
-
--- Create Student
-
--- Insert sample records
-
--- INNER JOIN query
+use collegeDBbca;
+CREATE TABLE Department(
+	departmentID INT PRIMARY KEY,
+    departmentName VARCHAR(50)
+);
+INSERT INTO department(departmentID,departmentName)VALUES (101,'Computer Science');
+INSERT INTO department(departmentID,departmentName)VALUES(102,'Mathematics');
+INSERT INTO department(departmentID,departmentName)VALUES(103,'Physics');
+CREATE TABLE student(
+	studentID INT PRIMARY KEY,
+    studentName VARCHAR(50),
+    departmentID INT 
+);
+INSERT INTO student VALUES(1001,'Arun',101);
+INSERT INTO student VALUES(1002,'Divya',102);
+INSERT INTO student VALUES(1003,'Karthik',101);
+INSERT INTO student VALUES(1004,'Nisha',103);
+SELECT student.studentName,department.departmentName
+FROM student
+INNER JOIN department
+ON student.DepartmentID=department.departmentID;
